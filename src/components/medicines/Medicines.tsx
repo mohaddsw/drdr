804aae8ee635331125e59f30364d83f3f288831b
Mohaddese@Rahimi <mohaddese.rahimi@shab.ir>
2025-03-12 13:51:38 +0330

    fix: fix layout

--- src/components/medicines/Medicines.tsx
+++ src/components/medicines/Medicines.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import styles from "./Medicines.module.css";
 import Card from "@/components/ui/card/Card";
 import Button from "@/components/ui/button/Button";
+import Image from "next/image";
+import menueImage from "../../../public/menue.png"
+import basketImage from "../../../public/basket.png"
 
 interface Medicine {
   id: number;
@@ -16,6 +19,10 @@ interface MedicinesPageProps {
 export default function Medicines({ medicines }: MedicinesPageProps) {
   return (
     <div className={styles.container}>
+      <div className={styles.nav}>
+        <Image alt="basket" src={basketImage} width={30} height={30} className={styles.images}/>
+        <Image alt="menue" src={menueImage} width={30} height={30} className={styles.images}/>
+      </div>
       <h1>لیست داروها</h1>
       <div className={styles.grid}>
         {medicines.map((medicine) => (
